385c535c2502c36abfd93b2cf58f8ca5404ff3fd
Allan Pineda Work <148286052+AP2celaque@users.noreply.github.com>
2024-08-23 10:59:45 -0600

    round robin

--- backend/bchApi.js
+++ backend/bchApi.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-const url = 'https://bchapi-am.azure-api.net/api/v1/indicadores/620/cifras?formato=Json&ordenamiento=desc&clave=acd6f33468194c77b46fd5397fb05e60';
+// const url = 'https://bchapi-am.azure-api.net/api/v1/indicadores/620/cifras?formato=Json&ordenamiento=desc&clave=acd6f33468194c77b46fd5397fb05e60';
+const url = 'https://bchapi-am.azure-api.net/api/v1/indicadores/620/cifras?formato=Json&fechaInicio=2017-01-01&ordenamiento=asc&clave=04d1c14361564cbfa78b0c37eb2398af';
 
 axios.get(url)
     .then(response => {
